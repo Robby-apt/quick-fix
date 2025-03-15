b1b632a4da669bf1e9ef7ea739cbb19b57fe125a
Robby <robin.kw99@yahoo.com>
2025-03-16 01:52:59 +0300

    Developing the global nav bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,70 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-function Navbar() {
+// import logo from '../../public/images/quick-fix.svg';
+
+function Navbar(props) {
 	return (
 		<nav className="globalNav">
 			<div className="branding">
-				<img src="" alt="Quick Fix logo" />
-				<h1>Quick Fix</h1>
+				<Link to="/">
+					<img
+						src={`${process.env.PUBLIC_URL}/images/quick-fix.svg`}
+						alt="Quick Fix logo"
+					/>
+					<h1>Quick Fix</h1>
+				</Link>
 			</div>
+
 			<div className="navLinks">
 				<Link to="/">Home</Link>
 				<Link to="/about">About Us</Link>
 				<Link to="/services">Services</Link>
 				<Link to="/contact">Contact</Link>
 			</div>
-			<div className="navLinksResponsive">
+
+			<div className="navIcons">
+				{props.isResNavShowing ? (
+					<i
+						className="fa-solid fa-xmark"
+						onClick={() => props.setResNavShowing(false)}
+					/>
+				) : (
+					<i
+						className="fa-solid fa-bars"
+						onClick={() => props.setResNavShowing(true)}
+					/>
+				)}
+			</div>
+
+			<div
+				className="navLinksResponsive"
+				style={
+					props.isResNavShowing
+						? { display: 'flex' }
+						: { display: 'none' }
+				}
+			>
 				<Link to="/">Home</Link>
 				<Link to="/about">About Us</Link>
 				<Link to="/services">Services</Link>
 				<Link to="/contact">Contact</Link>
+				<div className="regAndLogin">
+					<Link to="/login" className="loginBtn">
+						Login
+					</Link>
+					<Link to="/signup" className="signupBtn">
+						Sign Up
+					</Link>
+				</div>
 			</div>
-			<div className="regAndLogin">
-				<Link to="/login">Login</Link>
-				<Link to="/signup">Sign Up</Link>
+
+			<div className="regAndLoginDiv">
+				<Link to="/login" className="loginBtn">
+					Login
+				</Link>
+				<Link to="/signup" className="signupBtn">
+					Sign Up
+				</Link>
 			</div>
 		</nav>
 	);
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 
 function Landing() {
+	let [isResNavShowing, setResNavShowing] = useState(false);
 	return (
-		<div id="landingPage">
-			<Navbar />
+		<div className="page" id="landingPage">
+			<Navbar
+				isResNavShowing={isResNavShowing}
+				setResNavShowing={setResNavShowing}
+			/>
 		</div>
 	);
 }
